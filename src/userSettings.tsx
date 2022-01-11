ff0b9cc8bd38939521ecd4732bc822eebe3b798f
EikonaPatunga <90279677+EikonaPatunga@users.noreply.github.com>
2022-01-11 09:08:37 -0800

    wave#3

--- src/userSettings.tsx
+++ src/userSettings.tsx
@@ -138,14 +138,14 @@ export class MintWhitelistCustomHTML extends React.Component {
 export const publicSaleSettings: PublicSaleSettings = {
   //start date and end date must match your Candy Machine Config for public launch!
 
-  startDate: date("7 Jan 2022 00:00:00 GMT"),
+  startDate: date("11 Jan 2022 01:00:00 PM EST"),
   endDate: undefined,
   countdown: false,
   //Example date below
   // date('29 2021 00:00:00 GMT')
 
   //Title and Description
-  title: "Public Sale",
+  title: "Wave #3",
   desc: "Sale is now live. Mint your NFT below",
 
   //Enable Custom HTML
